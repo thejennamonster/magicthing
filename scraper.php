8de6667a42702c33ca0578869df48f9e5c745b76
jenna <thejennamonster@gmail.com>
2018-04-17 15:15:18 -0400

    Simpliest form so far. Works on one card.

--- scraper.php
+++ scraper.php
@@ -6,48 +6,39 @@
  * Time: 12:53 PM
  */
 
-#include 'db.php';
-
-$mysqli = new mysqli('10.1.14.35','magiccar_jenna','Boogerdot87','magiccar_carddb');
-
-/* check connection */
-echo "connected";
-
-if (mysqli_connect_errno()){
-    printf("Connect failed: %\n",mysqli_connect_errno());
-    exit();
-}
-//select database to work with
-
-$mysqli->select_db("magiccar_carddb");
+include 'db.php';
 
 require __DIR__ . '/vendor/autoload.php';
 
 use \mtgsdk\Card;
 
 $card = Card::find(439836);
-#$card = Card::where(['name' => 'nis'])->all();
+
 
 // spit out relevant information from datadump
 
-$multi= $card->multiverseid . "\n";
-$name = $card->name . "\n";
-$rarity = $card->rarity . "\n";
-$colors = implode(",", $card->colors) . "\n";
-$mana = $card->manaCost . "\n";
-$cmc = $card->cmc . "\n";
-$type = $card->type . "\n";
-$power = $card->power . "\n";
-$toughness = $card->toughness ."\n";
-$version= $card->setName . "\n";
-$text = $card->text . "\n";
-$image = $card->imageUrl ."\n";
+$card->multiverseid;
+$card->name;
+$card->rarity;
+$colors = implode(",", $card->colors);
+$card->manaCost;
+$card->cmc;
+$card->type;
+$card->power;
+$card->toughness;
+$card->setName;
+$card->text;
+$card->imageUrl;
 #die();
 
+//clear database to avoid multiple entries
+
+mysqli_query($mysqli, "TRUNCATE TABLE `cards`");
 
 //Write to database
 
-        $sql = "INSERT INTO cards (multiverseid, name, rarity, colors, mana, cmc, type, power, toughness, version, text, image) VALUES ('$multi','$name','$rarity','$colors','$mana','$cmc','$type','$power','$toughness','$version','$text','$image')";
+        $sql = "INSERT INTO cards (multiverseid, name, rarity, colors, mana, cmc, type, power, toughness, version, text, image) 
+                VALUES ('{$card->multiverseid}','{$card->name}','{$card->rarity}','$colors','{$card->manaCost}','{$card->cmc}','{$card->type}','{$card->power}','{$card->toughness}','{$card->setName}','{$card->text}','{$card->imageUrl}')";
 
 
          if ($mysqli->query($sql) === TRUE) {
